bdc4a297fb7da30292d3f4b503de16012dfb08e7
SYAN <syanays.git@gmail.com>
2024-12-09 21:40:47 +0300

    Add charts

--- controllers/admin/dashboard.php
+++ controllers/admin/dashboard.php
@@ -8,13 +8,38 @@ $config = require base_path('config.php');
 $db = new Database($config['database']); 
 
 // Query for room usage statistics
-$roomUsage = $db->query('SELECT room_id, COUNT(*) AS count FROM bookings')->fetch()['count'];
+$rooms_usage = $db->query('SELECT room_id, COUNT(room_id) AS bookings FROM bookings GROUP BY room_id')->fetchAll();
 
-// $scheduleCount = $conn->query("SELECT COUNT(*) AS count FROM schedules")->fetch()['count'];
+dump($rooms_usage);
+
+$bookings_today = $db->query("SELECT COUNT(*) AS count FROM bookings WHERE start_time = NOW() ")->fetch()['count'];
+
+dump($bookings_today);
+
+$bookings_week = $db->query("SELECT COUNT(*) AS count FROM bookings WHERE start_time = WEEKOFYEAR(from_unixtime(unix_timestamp())) ")->fetch()['count'];
+
+dump($bookings_today);
+
+// // Fetch bookings and related room names
+// try {
+//     $bookings = $db->query('SELECT b.*, r.room_name FROM bookings b JOIN rooms r ON b.room_id = r.room_id')->fetchAll();
+// } catch (Exception $e) {
+//     die('Error fetching bookings: ' . htmlspecialchars($e->getMessage()));
+// }
 
 
+// // Fetch all rooms
+// try {
+//     $rooms = $db->query('SELECT * FROM rooms')->fetchAll(PDO::FETCH_ASSOC);
+// } catch (Exception $e) {
+  
+//     die('Error fetching rooms: ' . htmlspecialchars($e->getMessage()));
+// }
+
+// Load the view
 view('admin/dashboard.view.php', [
     'h1' => 'Dashboard',
     'p' => 'Adminstrator',
-    'roomUsage' => $roomUsage
+    'rooms_usage' => $rooms_usage,
+    'bookings_today' 
 ]);
--- views/admin/dashboard.view.php
+++ views/admin/dashboard.view.php
@@ -1,23 +1,80 @@
 <?php require base_path('views/partials/head.php'); ?>
 <?php require base_path('views/partials/admin-header.php'); ?>
 
+<?php $rooms = [] ?>
+<?php $bookingsCount = [] ?>
+
 <section>
-    <h3>Most Booked Rooms</h3>
-    <!-- <ul>
-        <?php foreach ($roomUsage as $usage): ?>
-            <li>Room ID: <?php echo htmlspecialchars($usage['room_id']); ?> - Bookings: <?php echo htmlspecialchars($usage['bookings']); ?></li>
+    <h3>Room Bookings Statistics</h3>
+    <ul>
+        <?php foreach ($rooms_usage as $usage): ?>
+            <!-- <li>Room ID: <?php echo htmlspecialchars($usage['room_id']); ?> | Total Bookings: <?php echo htmlspecialchars($usage['bookings']); ?></li> -->
+            <?php array_push($rooms, $usage['room_id']) ?>
+            <?php array_push($bookingsCount, $usage['bookings']) ?>
         <?php endforeach; ?>
-    </ul> -->
-    <canvas id="mostBookedRooms" style="width:100%; max-width:700px"></canvas>
+    </ul>
+    <div class="chart-container">
+        <canvas id="barChart" style="max-width: 750px;"></canvas>
+        <canvas id="pieChart" width="200" height="100" style="max-width: 750px;"></canvas>
+    </div>
 </section>
 
-<?php print_r($roomUsage) ?>
-
 <script>
-    const mostBookedRooms = new Chart("mostBookedRooms", {
+    const xValues = <?= json_encode($rooms) ?>;
+    const yValues = <?= json_encode($bookingsCount) ?>;
+    const colors = [
+        "#25CCF7","#FD7272","#54a0ff","#00d2d3",
+        "#1abc9c","#2ecc71","#3498db","#9b59b6","#34495e",
+        "#16a085","#27ae60","#2980b9","#8e44ad","#2c3e50",
+        "#f1c40f","#e67e22","#e74c3c","#ecf0f1","#95a5a6",
+        "#f39c12","#d35400","#c0392b","#bdc3c7","#7f8c8d",
+        "#55efc4","#81ecec","#74b9ff","#a29bfe","#dfe6e9",
+        "#00b894","#00cec9","#0984e3","#6c5ce7","#ffeaa7",
+        "#fab1a0","#ff7675","#fd79a8","#fdcb6e","#e17055",
+        "#d63031","#feca57","#5f27cd","#54a0ff","#01a3a4"
+    ];
+
+    const barChart = new Chart("barChart", {
         type: "bar",
-        data: <?= json_encode($roomUsage) ?>,
-        options: {}
+        data: {
+            labels: xValues,
+            datasets: [{
+                backgroundColor: colors,
+                data: yValues
+            }]
+        },
+        options: {
+            "legend": {
+                "display": false
+            },
+
+            scales: {
+                yAxes:
+                [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+
+    const pieChart = new Chart("pieChart", {
+    type: "doughnut",
+    data: {
+        labels: xValues,
+        datasets:
+        [{
+            backgroundColor: colors,
+            data: yValues
+        }]
+    },
+    options: {
+        title: {
+        display: true,
+        text: "Most Booked Rooms"
+        }
+    }
     });
 </script>
 
